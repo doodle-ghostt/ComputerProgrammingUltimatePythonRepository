64053bf6c19c62c9946e1dd382ca4d164473db0e
Meenakshi Ghosh <27meenakshighosh@usd266.com>
2023-12-05 09:22:13 -0600

    lesson 6 finished

--- 06ListsAndLoops/Assignment/main.py
+++ 06ListsAndLoops/Assignment/main.py
@@ -34,8 +34,10 @@ def average_act_score(list):
         if score  >= 1 and score <= 36:
             count  = count + 1
             total  = total + score
-    total = total / count
-    return total
+    if count != 0:
+        total = total / count
+        return total
+    
 
 print("AVERAGE ACT")
 print(average_act_score([20, 18, 32, 60, 29, 101, 0]))
@@ -89,3 +91,49 @@ print(has_vowel(["a", "b", "n", "b", "b" ]))
 print(has_vowel(["k", "b", "n", "b", "b" ]))
 print(has_vowel(["f", "b", "n", "u", "b" ]))
 print(has_vowel(["e", "s", "o", "i", "a" ]))
+
+def all_the_same(ints):
+    goal = ints[0]
+    for int in ints:
+        if int != goal:
+            return False
+    return True
+
+def increasing(ints):
+    prev = ints[0]
+    for current in ints[1:]:
+        if current <= prev:
+            return False
+        else:
+            prev = current
+    return True
+
+def is_incrementing(ints):
+    prev = ints[0]
+    for int in ints[1:]:
+        if int - prev != 1: 
+            return False
+        else:
+            prev = int
+    return True
+
+def has_adjacent_repeat(list1):
+    prev = list1[0]
+    for num in list1[1:]:
+        if num == prev:
+            return True
+        else:
+            prev = num
+    return False
+
+def sum_with_skips(list1):
+    skip = False
+    total = 0
+    for num in list1:
+        if num == -1 and skip == False:
+            skip = True
+        elif num == -1 and skip == True:
+            skip = False
+        elif skip == False:
+            total = total + num
+    return total
