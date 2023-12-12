f9185d71ac724ec460e337c65a30de90b84d3f57
Meenakshi Ghosh <27meenakshighosh@usd266.com>
2023-12-12 09:38:06 -0600

    finished some

--- 09WorkingWithFiles/Assignment/main.py
+++ 09WorkingWithFiles/Assignment/main.py
@@ -1,1 +1,23 @@
 import csv
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+#f = open(r'09WorkingWithFiles\data\4000-most-common-english-words.csv', "r")
+f = open("../data/4000-most-common-english-words.csv", "r")
+words = csv.reader(f)
+
+def most_vowels(wordlist):
+    most_vowels = ""
+    prev_vowel_count = 0
+    current_vowel_count = 0
+    for row in wordlist:
+        for word in row:
+            for letter in word:
+                if letter in "aeiou":
+                    current_vowel_count += 1
+            if current_vowel_count > prev_vowel_count:
+                most_vowels = word
+                prev_vowel_count = current_vowel_count
+                current_vowel_count = 0
+    return most_vowels
+
+most_vowels(words)
